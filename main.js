1cca9e43da129644d73679c5be78aed3b98bcce4
varchasvi-ai <76240712+varchasvi-ai@users.noreply.github.com>
2021-10-09 19:40:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+function preload()
+{
+    song = loadSound("music.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(600,400);
+    canvas.center();
+
+
+    video = createCapture(VIDEO);
+    video.hide;
+    
+
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose' , gotPoses);
+
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet has been initialized");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X = " + leftWristX + "Left Wrist Y = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X = " + rightWristX + "Right Wrist Y = " + rightWristY);
+
+    }
+}
+
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function pause()
+{
+    song.pause();
+}
+
+function stop()
+{
+    song.stop();
+}
